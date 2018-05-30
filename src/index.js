578a9247b3f2c62bf402c831781a853465372041
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-05-30 15:30:27 +0500

    Simplest Structure. API youtube key also placed

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const API_KEY = "AIzaSyA5iQPZiyFRIADX5xUZealbrmAiKU8xz3A";
+
+const App = ()=> {
+	return <div>Hello World!</div>;
+}
+
+
+ReactDom.render(<App />, document.querySelector('.container'));
