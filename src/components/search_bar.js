d62373280559e369e7a3bf25df210186d9eae6cd
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-05-30 22:31:28 +0500

    Event handling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react'  //Or const Component = React.Component
 
 class SearchBar extends Component{
     render(){
-        return <input />
+        return <input onChange={this.onInputChange} />
+    }
+
+    onInputChange(event){
+        console.log(event.target.value);
     }
 }
 
