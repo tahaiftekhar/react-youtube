0448adacfdff9e8014f469e41d807b5519f2f8d2
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-05-31 00:04:40 +0500

    Made use of YTSearch Library and made basic videos search on Youtube Changed App from Function component to Class component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = "AIzaSyA5iQPZiyFRIADX5xUZealbrmAiKU8xz3A";
 
-const App = ()=> {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-}
+class App extends Component{
+	constructor(props){
+		super(props);
 
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=> {
+			this.setState({ videos });
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
 
 ReactDom.render(<App />, document.querySelector('.container'));
