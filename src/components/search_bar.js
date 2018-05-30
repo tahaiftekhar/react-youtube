b2f9e13e031eea6e789ad4c909c708a20e1bd932
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-05-30 23:27:38 +0500

    Oneline Event handling. State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react'  //Or const Component = React.Component
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = { term: '' };
+    }
     render(){
-        return <input onChange={this.onInputChange} />
+        return (
+            <div>
+                <input onChange={(e)=>this.setState({term: e.target.value})} />
+                <br />
+                <h2>Value: {this.state.term}</h2>
+            </div>
+        );
     }
 
-    onInputChange(event){
-        console.log(event.target.value);
-    }
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    //     this.setState({term: event.target.value});
+    // }
 }
 
 export default SearchBar;
