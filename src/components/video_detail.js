e3c4066c9e275efdf16855bc5d484b397fe4e32b
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-05-31 15:43:50 +0500

    corrected spelling

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -7,7 +7,7 @@ const VideoDetail = ({video})=> {
     //Same as const url = 'https://youtube.com/embed/' + videoId;
 
     return (
-        <div classname="video-detail col-md-8">
+        <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
                 <iframe className="embed-responive-item" src={url}></iframe>
             </div>
