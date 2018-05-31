79917daf7ea848bc88f0eecca38b6ad59cd717fd
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-05-31 14:34:09 +0500

    Video detail made

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = "AIzaSyA5iQPZiyFRIADX5xUZealbrmAiKU8xz3A";
 
@@ -11,10 +12,16 @@ class App extends Component{
 	constructor(props){
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = {
+			videos: [],
+            selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=> {
-			this.setState({ videos });
+			this.setState({
+				videos: videos,
+                selectedVideo: videos[0]
+			});
 			console.log(this.state.videos);
 		});
 	}
@@ -23,6 +30,7 @@ class App extends Component{
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList videos={this.state.videos} />
 			</div>
 		);
