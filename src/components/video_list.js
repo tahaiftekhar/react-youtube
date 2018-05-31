814d1d7e1e5da13a819e7e345cfa2332c1dde7eb
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-05-31 15:49:27 +0500

    Clicking on videos list result in the Video Detail change accordingly

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,13 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     const videoListItems = props.videos.map((video)=> {
-        return (<VideoListItem key={video.etag} video={video}/>);
+        return (
+            <VideoListItem
+                key={video.etag}
+                video={video}
+                onVideoSelect={props.onVideoSelect}
+            />
+            );
     });
     return (
         <ul className="col-md-4 list-group">
